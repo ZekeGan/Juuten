72645aadb878b644b5a662fca43a155e089e9be1
Zeke <traveller870720@gmail.com>
2023-05-01 21:08:43 +0800

    bugfix conflict error

--- src/page/Router.jsx
+++ src/page/Router.jsx
@@ -4,16 +4,6 @@ import Home from './folder/Main.jsx'
 import Collection from './collection/Main.jsx'
 
 export default function App() {
-<<<<<<< HEAD
-
-    console.log('router');
-<<<<<<< HEAD
-    console.log('test');
-=======
->>>>>>> b6dc3fac43a93fd134125dd541fd013b97eb8160
-
-=======
->>>>>>> 2a3a319 (501 OPT)
     return (
         <Router>
             <Routes>
